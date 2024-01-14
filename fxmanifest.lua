b83c5e4aec26b936aa9af56d366184cf6057e18e
Mike <156581664+Semtexdev@users.noreply.github.com>
2024-01-14 12:39:52 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Semtexdev'
+description 'Attachments Script (FREE)'
+version '1.0.0'
+
+client_script {
+    'client.lua',
+    'config.lua' 
+}
+server_script {
+    'server.lua',
+    'config.lua'
+}
+
+shared_script {
+    '@es_extended/imports.lua'
+}
